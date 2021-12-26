50f7e2db684a63ce5dd351f07195150671ee841f
unkoqn01 <recreater35@gmail.com>
2021-12-26 16:35:18 +0900

    12-26

--- lecture_code/java_sts/src/SpringBootBoard/main/java/com/bitc/board/dto/BoardDto.java
+++ lecture_code/java_sts/src/SpringBootBoard/main/java/com/bitc/board/dto/BoardDto.java
@@ -2,6 +2,10 @@ package com.bitc.board.dto;
 
 import lombok.Data;
 
+// 롬복이란?
+// 데이터 베이스와 매핑되는 dto 객체를 생성 시 getter/setter를 자동으로 생성해줌
+
+// getter/setter/toString 메서드를 자동으로 생성해 줌
 @Data
 public class BoardDto {
 
